0b4b1957cfd090cd64c770d614acb90f659c0407
NoahTTR <31555301+NoahTTR@users.noreply.github.com>
2023-12-28 17:30:54 -0600

    Frame and sortingPanel setup

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Frame frame = new Frame();
+        SortingPanel sortingPanel = new SortingPanel(50);
+        frame.add(sortingPanel);
+
+    }
+}
