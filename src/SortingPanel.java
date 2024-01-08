c1c00357cf43b22ad01f5628e57dd20c7d47a622
NoahTTR <31555301+NoahTTR@users.noreply.github.com>
2024-01-07 21:58:01 -0600

    Bubble sort animation complete

--- src/SortingPanel.java
+++ src/SortingPanel.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -8,13 +10,22 @@ import java.util.Random;
  * The SortingPanel class that creates a JPanel which includes
  * all the array functionality and graphics.
  */
-public class SortingPanel extends JPanel {
+public class SortingPanel extends JPanel implements ActionListener {
 
     final int MIN_VALUE = 5;
     final int MAX_VALUE = 500;
     private final int[] array;
     private final ArrayList<JLabel> arrayLabels;
     private final Sort sort;
+    Timer checkTimer;
+    Timer swtichTimer;
+    boolean bubbleSortRunning;
+    boolean switchElements;
+    int selectedIndex1;
+    int selectedIndex2;
+    int indexVelocity;
+    int innerLoopCompletions;
+    int uncheckedElements;
 
     public SortingPanel(int arraySize) {
 
@@ -22,10 +33,20 @@ public class SortingPanel extends JPanel {
         array = new int[arraySize];
         arrayLabels = new ArrayList<>(arraySize);
         sort = new Sort();
+        bubbleSortRunning = false;
+        switchElements = false;
+        selectedIndex1 = -1;
+        selectedIndex2 = -1;
+        indexVelocity = 0;
+        innerLoopCompletions = 0;
+        uncheckedElements = 0;
 
         // Generate random array
         this.generateArray(MIN_VALUE, MAX_VALUE);
         this.displayArray();
+
+        checkTimer = new Timer(100, this);
+        swtichTimer = new Timer(0, this);
     }
 
     public void paintComponent(Graphics g) {
@@ -40,8 +61,33 @@ public class SortingPanel extends JPanel {
         int rectY = this.getSize().height;
 
         for (int i = 0; i < array.length; i++) {
-            g2d.drawRect(rectX + space, rectY, 20, -(array[i]));
-            g2d.fillRect(rectX + space, rectY, 20, -(array[i]));
+            if (i == selectedIndex1 || i == selectedIndex2) {
+                g2d.setColor(Color.green);
+                if (switchElements) {
+                    if (i == selectedIndex1) {
+                        g2d.drawRect(rectX + space + indexVelocity, rectY, 20, -(array[i]));
+                        g2d.fillRect(rectX + space + indexVelocity, rectY, 20, -(array[i]));
+                    }
+                    else {
+                        g2d.drawRect(rectX + space - indexVelocity, rectY, 20, -(array[i]));
+                        g2d.fillRect(rectX + space - indexVelocity, rectY, 20, -(array[i]));
+                    }
+                }
+                else {
+                    g2d.drawRect(rectX + space, rectY, 20, -(array[i]));
+                    g2d.fillRect(rectX + space, rectY, 20, -(array[i]));
+                }
+            }
+            else {
+                if (array.length - innerLoopCompletions <= i) {
+                    g2d.setColor(Color.orange);
+                }
+                else {
+                    g2d.setColor(new Color(0, 150, 255));
+                }
+                g2d.drawRect(rectX + space, rectY, 20, -(array[i]));
+                g2d.fillRect(rectX + space, rectY, 20, -(array[i]));
+            }
             space += 30;
         }
     }
@@ -71,10 +117,49 @@ public class SortingPanel extends JPanel {
     }
 
     public void bubbleSortArray() {
-        sort.bubbleSort(array);
-        System.out.println(Arrays.toString(array));
-        for (int i = 0; i < arrayLabels.size(); i++) {
-            arrayLabels.get(i).setText(Integer.toString(array[i]));
+        bubbleSortRunning = true;
+        uncheckedElements = array.length;
+        selectedIndex1 = 0;
+        selectedIndex2 = 1;
+        checkTimer.start();
+        repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (array[selectedIndex1] > array[selectedIndex2]) {
+            if (indexVelocity >= 30) {
+                switchElements = false;
+                int temp = array[selectedIndex1];
+                array[selectedIndex1] = array[selectedIndex2];
+                array[selectedIndex2] = temp;
+                swtichTimer.stop();
+                checkTimer.start();
+                indexVelocity = 0;
+            }
+            else {
+                switchElements = true;
+                checkTimer.stop();
+                swtichTimer.start();
+                indexVelocity++;
+            }
+        }
+        else if (uncheckedElements <= 1) {
+            bubbleSortRunning = false;
+            selectedIndex1 = selectedIndex2 = -1;
+            innerLoopCompletions = 0;
+            uncheckedElements = 0;
+            checkTimer.stop();
+        }
+        else if (selectedIndex2 >= uncheckedElements - 1) {
+            innerLoopCompletions++;
+            uncheckedElements--;
+            selectedIndex1 = 0;
+            selectedIndex2 = 1;
+        }
+        else {
+            selectedIndex1++;
+            selectedIndex2++;
         }
         repaint();
     }
