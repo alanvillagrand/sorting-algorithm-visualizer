2b8f97c1c9cb8012a70f6221d93d2c2d04bfe046
NoahTTR <31555301+NoahTTR@users.noreply.github.com>
2024-01-11 23:15:41 -0600

    Added speed panel, needs more work

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
-import javax.swing.border.Border;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,18 +10,21 @@ import java.util.Arrays;
  * The ControlPanel class that creates a JPanel which includes all
  * the control functionality and graphics.
  */
-public class ControlPanel extends JPanel implements ActionListener {
+public class ControlPanel extends JPanel implements ActionListener, ChangeListener {
 
     SortingPanel sortingPanel;
 
     JPanel rightPanel;
     JPanel leftPanel;
+    JPanel speedPanel;
 
     JButton generateArrButton;
     JButton bubbleSortButton;
     JButton insertionSortButton;
     JButton quickSortButton;
     JButton mergeSortButton;
+    JButton pauseResumeButton;
+    JButton skipButton;
 
     JLabel arrayLabel;
     JLabel sizeLabel;
@@ -28,9 +32,14 @@ public class ControlPanel extends JPanel implements ActionListener {
     JLabel insertionRunTimeLabel;
     JLabel quickRunTimeLabel;
     JLabel mergeRunTimeLabel;
+    JLabel speedLabel;
 
     JTextField arrayField;
     JTextField sizeField;
+
+    JSlider speedSlider;
+
+    boolean paused;
 
     public ControlPanel(SortingPanel sortingPanel) {
         // Set panel attributes
@@ -38,32 +47,78 @@ public class ControlPanel extends JPanel implements ActionListener {
         this.setPreferredSize(new Dimension(100, 100));
         this.setLayout(new BorderLayout());
 
+        paused = false;
+
         // Create Panels
         this.sortingPanel = sortingPanel;
 
         rightPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
         rightPanel.setBackground(new Color(0x1E3D57));
-        rightPanel.setPreferredSize(new Dimension(450, 100));
+        rightPanel.setPreferredSize(new Dimension(520, 100));
 
         leftPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 10));
         //leftPanel.setPreferredSize(new Dimension(200, 100));
         leftPanel.setBackground(new Color(0x1E3D57));
 
+        speedPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 10));
+        speedPanel.setBackground(new Color(0x1E3D57));
+
         // Create buttons
         generateArrButton = new JButton("Generate new array");
+        generateArrButton.setFont(new Font("Arial", Font.BOLD, 14));
+        generateArrButton.setForeground(Color.white);
+        generateArrButton.setBackground(new Color(66, 139, 202));
+        generateArrButton.setFocusPainted(false);
+        generateArrButton.setBorderPainted(false);
         generateArrButton.addActionListener(this);
 
         bubbleSortButton = new JButton("Bubble Sort");
+        bubbleSortButton.setFont(new Font("Arial", Font.BOLD, 14));
+        bubbleSortButton.setForeground(Color.white);
+        bubbleSortButton.setBackground(new Color(66, 139, 202));
+        bubbleSortButton.setFocusPainted(false);
+        bubbleSortButton.setBorderPainted(false);
         bubbleSortButton.addActionListener(this);
 
         insertionSortButton = new JButton("Insertion Sort");
+        insertionSortButton.setFont(new Font("Arial", Font.BOLD, 14));
+        insertionSortButton.setForeground(Color.white);
+        insertionSortButton.setBackground(new Color(66, 139, 202));
+        insertionSortButton.setFocusPainted(false);
+        insertionSortButton.setBorderPainted(false);
         insertionSortButton.addActionListener(this);
 
         quickSortButton = new JButton("Quick Sort");
+        quickSortButton.setFont(new Font("Arial", Font.BOLD, 14));
+        quickSortButton.setForeground(Color.white);
+        quickSortButton.setBackground(new Color(66, 139, 202));
+        quickSortButton.setFocusPainted(false);
+        quickSortButton.setBorderPainted(false);
         quickSortButton.addActionListener(this);
 
         mergeSortButton = new JButton("Merge Sort");
+        mergeSortButton.setFont(new Font("Arial", Font.BOLD, 14));
+        mergeSortButton.setForeground(Color.white);
+        mergeSortButton.setBackground(new Color(66, 139, 202));
+        mergeSortButton.setFocusPainted(false);
+        mergeSortButton.setBorderPainted(false);
         mergeSortButton.addActionListener(this);
+
+        pauseResumeButton = new JButton("Pause");
+        pauseResumeButton.setFont(new Font("Arial", Font.BOLD, 14));
+        pauseResumeButton.setForeground(Color.white);
+        pauseResumeButton.setBackground(new Color(66, 139, 202));
+        pauseResumeButton.setFocusPainted(false);
+        pauseResumeButton.setBorderPainted(false);
+        pauseResumeButton.addActionListener(this);
+
+        skipButton = new JButton("Skip");
+        skipButton.setFont(new Font("Arial", Font.BOLD, 14));
+        skipButton.setForeground(Color.white);
+        skipButton.setBackground(Color.RED);
+        skipButton.setFocusPainted(false);
+        skipButton.setBorderPainted(false);
+        skipButton.addActionListener(this);
 
         //Create labels
         arrayLabel = new JLabel("Array =");
@@ -71,6 +126,9 @@ public class ControlPanel extends JPanel implements ActionListener {
 
         sizeLabel = new JLabel("Size =");
         sizeLabel.setForeground(Color.white);
+
+        speedLabel = new JLabel("Speed =");
+        speedLabel.setForeground(Color.white);
 
         bubbleRunTimeLabel = new JLabel("          O(n^2)          ");
         bubbleRunTimeLabel.setForeground(Color.white);
@@ -92,8 +150,13 @@ public class ControlPanel extends JPanel implements ActionListener {
 
         sizeField = new JTextField();
         sizeField.setPreferredSize(new Dimension(30, 20));
-        sizeField.setText(String.valueOf(sortingPanel.getArray().length));
         sizeField.addActionListener(this);
+        this.updateSizeField();
+
+        // Create slider
+        speedSlider = new JSlider(0, 1000, 0);
+        speedSlider.addChangeListener(this);
+        speedSlider.setBackground(new Color(0x1E3D57));
 
         // Add buttons to panels
         rightPanel.add(bubbleSortButton);
@@ -111,9 +174,16 @@ public class ControlPanel extends JPanel implements ActionListener {
         leftPanel.add(sizeLabel);
         leftPanel.add(sizeField);
 
+        speedPanel.add(speedLabel);
+        speedPanel.add(speedSlider);
+        speedPanel.add(pauseResumeButton);
+        speedPanel.add(skipButton);
+
         // Add panels to main panel
         this.add(leftPanel, BorderLayout.WEST);
+        this.add(speedPanel, BorderLayout.CENTER);
         this.add(rightPanel, BorderLayout.EAST);
+        this.hideSpeedControls();
     }
 
     private void updateArrayField() {
@@ -121,14 +191,40 @@ public class ControlPanel extends JPanel implements ActionListener {
         arrayField.setText(arrayString.substring(1, arrayString.length() - 1));
     }
 
+    private void updateSizeField() {
+        sizeField.setText(String.valueOf(sortingPanel.getArray().length));
+    }
+
     private int[] stringToArray(String str) {
-        char[] charArray = str.toCharArray();
-        int[] intArray = new int[charArray.length];
-        for (int i = 0; i < charArray.length; i++) {
-            System.out.print(charArray[i]);
-            System.out.print(" ");
+        String[] strValues = str.split(",");
+        for (int i = 0; i < strValues.length; i++) {
+            strValues[i] = strValues[i].trim();
         }
+
+        int[] intArray = new int[strValues.length];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = Integer.parseInt(strValues[i]);
+        }
+
         return intArray;
+    }
+
+    private void hideMainControls() {
+        leftPanel.hide();
+        rightPanel.hide();
+    }
+
+    private void showMainControls() {
+        leftPanel.show();
+        rightPanel.show();
+    }
+
+    private void hideSpeedControls() {
+        speedPanel.hide();
+    }
+
+    private void showSpeedControls() {
+        speedPanel.show();
     }
 
     @Override
@@ -138,15 +234,45 @@ public class ControlPanel extends JPanel implements ActionListener {
             this.updateArrayField();
         }
         else if (e.getSource() == arrayField) {
-            int[] array = stringToArray(arrayField.getText());
+            int[] newArray = stringToArray(arrayField.getText());
+            sortingPanel.updateArray(newArray);
+            this.updateArrayField();
+            this.updateSizeField();
 
         }
         else if (e.getSource() == sizeField) {
             sortingPanel.resizeArray(Integer.parseInt(sizeField.getText()));
             this.updateArrayField();
+            this.updateSizeField();
         }
         else if (e.getSource() == bubbleSortButton) {
             sortingPanel.bubbleSortArray();
+            this.hideMainControls();
+            this.showSpeedControls();
         }
+        else if (e.getSource() == pauseResumeButton) {
+            if (paused){
+                sortingPanel.resumeSort();
+                pauseResumeButton.setBackground(new Color(66, 139, 202));
+                pauseResumeButton.setText("Pause");
+                paused = false;
+            }
+            else {
+                sortingPanel.pauseSort();
+                pauseResumeButton.setBackground(new Color(66, 202, 109));
+                pauseResumeButton.setText("Resume");
+                paused = true;
+            }
+        }
+        else if (e.getSource() == skipButton) {
+            sortingPanel.skipSort();
+            this.hideSpeedControls();
+            this.showMainControls();
+        }
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        sortingPanel.setSpeed(1000 - speedSlider.getValue());
     }
 }
--- src/SortingPanel.java
+++ src/SortingPanel.java
@@ -17,7 +17,7 @@ public class SortingPanel extends JPanel implements ActionListener {
     private int[] array;
     private final Sort sort;
     Timer checkTimer;
-    Timer swtichTimer;
+    Timer switchTimer;
     boolean bubbleSortRunning;
     boolean switchElements;
     int selectedIndex1;
@@ -25,6 +25,7 @@ public class SortingPanel extends JPanel implements ActionListener {
     int indexVelocity;
     int innerLoopCompletions;
     int uncheckedElements;
+    int speed;
 
     public SortingPanel(int arraySize) {
 
@@ -38,12 +39,13 @@ public class SortingPanel extends JPanel implements ActionListener {
         indexVelocity = 0;
         innerLoopCompletions = 0;
         uncheckedElements = 0;
+        speed = 1000;
 
         // Generate random array
         this.generateArray(MIN_VALUE, MAX_VALUE);
 
-        checkTimer = new Timer(1000, this);
-        swtichTimer = new Timer(0, this);
+        checkTimer = new Timer(speed, this);
+        switchTimer = new Timer(0, this);
     }
 
     public void paintComponent(Graphics g) {
@@ -54,25 +56,28 @@ public class SortingPanel extends JPanel implements ActionListener {
 
         // Draw array values as rectangles
         int space = 0;
-        int rectX = (this.getSize().width / 2) - (array.length * 10 + 30);
+        int width = 20;
+        int rectX = (this.getSize().width / 2) - 10 - ((array.length - 1) * 15);
         int rectY = this.getSize().height;
+
+        //g2d.drawLine(this.getSize().width / 2, 100, this.getSize().width / 2, this.getSize().height);
 
         for (int i = 0; i < array.length; i++) {
             if (i == selectedIndex1 || i == selectedIndex2) {
                 g2d.setColor(Color.green);
                 if (switchElements) {
                     if (i == selectedIndex1) {
-                        g2d.drawRect(rectX + space + indexVelocity, rectY, 20, -(array[i]));
-                        g2d.fillRect(rectX + space + indexVelocity, rectY, 20, -(array[i]));
+                        g2d.drawRect(rectX + space + indexVelocity, rectY, width, -(array[i]));
+                        g2d.fillRect(rectX + space + indexVelocity, rectY, width, -(array[i]));
                     }
                     else {
-                        g2d.drawRect(rectX + space - indexVelocity, rectY, 20, -(array[i]));
-                        g2d.fillRect(rectX + space - indexVelocity, rectY, 20, -(array[i]));
+                        g2d.drawRect(rectX + space - indexVelocity, rectY, width, -(array[i]));
+                        g2d.fillRect(rectX + space - indexVelocity, rectY, width, -(array[i]));
                     }
                 }
                 else {
-                    g2d.drawRect(rectX + space, rectY, 20, -(array[i]));
-                    g2d.fillRect(rectX + space, rectY, 20, -(array[i]));
+                    g2d.drawRect(rectX + space, rectY, width, -(array[i]));
+                    g2d.fillRect(rectX + space, rectY, width, -(array[i]));
                 }
             }
             else {
@@ -82,10 +87,10 @@ public class SortingPanel extends JPanel implements ActionListener {
                 else {
                     g2d.setColor(new Color(0, 150, 255));
                 }
-                g2d.drawRect(rectX + space, rectY, 20, -(array[i]));
-                g2d.fillRect(rectX + space, rectY, 20, -(array[i]));
+                g2d.drawRect(rectX + space, rectY, width, -(array[i]));
+                g2d.fillRect(rectX + space, rectY, width, -(array[i]));
             }
-            space += 30;
+            space = space + 30;
         }
     }
 
@@ -102,7 +107,8 @@ public class SortingPanel extends JPanel implements ActionListener {
     }
 
     public void updateArray(int[] newArray) {
-        
+        this.array = newArray;
+        repaint();
     }
 
     public void resizeArray(int newSize) {
@@ -119,6 +125,43 @@ public class SortingPanel extends JPanel implements ActionListener {
 
     public int[] getArray() {
         return array;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+        checkTimer.stop();
+        checkTimer = new Timer(this.speed, this);
+        if (bubbleSortRunning && !switchElements) {
+            checkTimer.start();
+        }
+    }
+
+    public void pauseSort() {
+        if (switchElements) {
+            switchTimer.stop();
+        }
+        else {
+            checkTimer.stop();
+        }
+    }
+
+    public void resumeSort() {
+        if (switchElements) {
+            switchTimer.start();
+        }
+        else {
+            checkTimer.start();
+        }
+    }
+
+    public void skipSort() {
+        checkTimer.stop();
+        switchTimer.stop();
+        bubbleSortRunning = false;
+        selectedIndex1 = selectedIndex2 = -1;
+        innerLoopCompletions = uncheckedElements = 0;
+        sort.bubbleSort(array);
+        repaint();
     }
 
     public void bubbleSortArray() {
@@ -138,15 +181,35 @@ public class SortingPanel extends JPanel implements ActionListener {
                 int temp = array[selectedIndex1];
                 array[selectedIndex1] = array[selectedIndex2];
                 array[selectedIndex2] = temp;
-                swtichTimer.stop();
+                switchTimer.stop();
                 checkTimer.start();
                 indexVelocity = 0;
             }
             else {
                 switchElements = true;
                 checkTimer.stop();
-                swtichTimer.start();
-                indexVelocity++;
+                switchTimer.start();
+                if (this.speed > 900) {
+                    indexVelocity += 1;
+                } else if (this.speed > 800) {
+                    indexVelocity += 3;
+                } else if (this.speed > 700) {
+                    indexVelocity += 6;
+                } else if (this.speed > 600) {
+                    indexVelocity += 9;
+                } else if (this.speed > 500) {
+                    indexVelocity += 12;
+                } else if (this.speed > 400) {
+                    indexVelocity += 15;
+                } else if (this.speed > 300) {
+                    indexVelocity += 18;
+                } else if (this.speed > 200) {
+                    indexVelocity += 21;
+                } else if (this.speed > 100) {
+                    indexVelocity += 24;
+                } else {
+                    indexVelocity += 27;
+                }
             }
         }
         else if (uncheckedElements <= 1) {
